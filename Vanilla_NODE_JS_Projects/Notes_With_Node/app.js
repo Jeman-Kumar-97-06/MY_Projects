4b5691037b7070a2387d1c2352f45feaf5ae1e91
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2023-12-24 19:36:11 +0530

    Day 5 Vanilla_NODE_JS Project -- Notes App  -- 24/12/23

--- Vanilla_NODE_JS_Projects/Notes_With_Node/app.js
+++ Vanilla_NODE_JS_Projects/Notes_With_Node/app.js
@@ -4,6 +4,7 @@ const app     = express();
 const dbUri   = "mongodb+srv://mrj06031997:yUU73fcquguPC3Su@netninjacluster.vih7jsc.mongodb.net/?retryWrites=true&w=majority";
 app.set('view engine','ejs');
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}));
 mong.connect(dbUri,{useNewUrlParser:true,useUnifiedTopology:true}).then(result=>{
     console.log('connected successfully to database');
     app.listen(3000,()=>{console.log('listening at 3000')})
@@ -12,5 +13,21 @@ mong.connect(dbUri,{useNewUrlParser:true,useUnifiedTopology:true}).then(result=>
 })
 
 app.get('/',(req,res)=>{
-    res.render('create')
+    res.render('index')
+})
+
+app.get('/create-note',(req,res)=>{
+    res.render('create');
+})
+
+app.get('/create-list',(req,res)=>{
+    res.render('create_list');
+})
+
+app.post('/notes-normal',(req,res)=>{
+    console.log(req.body)
+})
+
+app.post('/notes-list',(req,res)=>{
+    console.log(req.body)
 })
