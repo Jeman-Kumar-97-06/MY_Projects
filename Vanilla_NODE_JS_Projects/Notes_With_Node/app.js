4f588f9958519e0f025f234c51dba62c3d344f7a
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2023-12-24 18:33:55 +0530

    Day 5 Vanilla_NODE_JS Project -- Notes App  -- 24/12/23

--- Vanilla_NODE_JS_Projects/Notes_With_Node/app.js
+++ Vanilla_NODE_JS_Projects/Notes_With_Node/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mong    = require('mongoose');
 const app     = express();
 const dbUri   = "mongodb+srv://mrj06031997:yUU73fcquguPC3Su@netninjacluster.vih7jsc.mongodb.net/?retryWrites=true&w=majority";
 app.set('view engine','ejs');
 app.use(express.static('public'))
-app.listen(3000,()=>{
-    console.log('listening at 3000');
-});
+mong.connect(dbUri,{useNewUrlParser:true,useUnifiedTopology:true}).then(result=>{
+    console.log('connected successfully to database');
+    app.listen(3000,()=>{console.log('listening at 3000')})
+}).catch(err=>{
+    console.log(err);
+})
+
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.render('create')
 })
