67d9b10f3f9bcb23a750ae70f3cd84f9ef23db9c
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2023-12-24 12:05:44 +0530

    Day 5 Vanilla_JS Project -- Color_Flipper -- 24/12/23

--- Vanilla_NODE_JS_Projects/Color_Flipper/code.js
+++ Vanilla_NODE_JS_Projects/Color_Flipper/code.js
@@ -1,1 +1,7 @@
 let body = document.querySelector('.bod');
+let btn  = document.querySelector('.click');
+let clr  = document.querySelector('.color-info');
+let bod  = document.querySelector('.bod');
+btn.addEventListener('click',(e)=>{
+    console.log(document.querySelector('body'))
+})
