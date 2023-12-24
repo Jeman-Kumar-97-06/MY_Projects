f81db6b93441e8d76864d048a66fc67b836cde8a
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2023-12-24 16:20:52 +0530

    Day 5 Vanilla_NODE_JS Project -- Notes App  -- 24/12/23

--- Vanilla_NODE_JS_Projects/Notes_With_Node/app.js
+++ Vanilla_NODE_JS_Projects/Notes_With_Node/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app     = express();
+const dbUri   = "mongodb+srv://mrj06031997:yUU73fcquguPC3Su@netninjacluster.vih7jsc.mongodb.net/?retryWrites=true&w=majority";
 app.set('view engine','ejs');
+app.use(express.static('public'))
 app.listen(3000,()=>{
     console.log('listening at 3000');
 });
