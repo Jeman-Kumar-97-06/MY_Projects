9e8643eb3bbaf1a77f9cb40b13528cb832a179d1
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2023-12-25 20:24:39 +0530

    Day 6 Vanilla_NODE_JS Project -- Notes App [Writing code for MongoDB]  -- 25/12/23

--- Vanilla_NODE_JS_Projects/Notes_With_Node/app.js
+++ Vanilla_NODE_JS_Projects/Notes_With_Node/app.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mong    = require('mongoose');
 const app     = express();
-const dbUri   = "mongodb+srv://mrj06031997:yUU73fcquguPC3Su@netninjacluster.vih7jsc.mongodb.net/?retryWrites=true&w=majority";
+const dbUri   = "mongodb+srv://mrj06031997:yUU73fcquguPC3Su@netninjacluster.vih7jsc.mongodb.net/note-tuts?retryWrites=true&w=majority";
 app.set('view engine','ejs');
 app.use(express.static('public'))
 app.use(express.urlencoded({extended:true}));
 mong.connect(dbUri,{useNewUrlParser:true,useUnifiedTopology:true}).then(result=>{
     console.log('connected successfully to database');
-    app.listen(3000,()=>{console.log('listening at 3000')})
+    app.listen(3000,()=>{console.log('listening at 3000')});
 }).catch(err=>{
     console.log(err);
 })
@@ -24,10 +24,11 @@ app.get('/create-list',(req,res)=>{
     res.render('create_list');
 })
 
+//Add new notes
 app.post('/notes-normal',(req,res)=>{
-    console.log(req.body)
+    console.log(req.body);
 })
 
 app.post('/notes-list',(req,res)=>{
-    console.log(req.body)
+    console.log(req.body);
 })
