e141678992be651282f23fc83edc4c4982a935ec
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-01-10 11:08:38 +0530

    Day 14 Added new HTML_CSS Project -- [Hover_Tabs] -- 10/01/2024

--- React_JS Projects/react_blog/src/Home.js
+++ React_JS Projects/react_blog/src/Home.js
@@ -1,5 +1,5 @@
-import { useState } from 'react'; 
-
+import { useState,useEffect } from 'react'; 
+import BlogList from './BlogList';
 const Home = () => {
     const [blogs,setBlogs] = useState(
         [
@@ -8,14 +8,16 @@ const Home = () => {
             {title:"Blog 3",body:"Blog3 body Blog3 body Blog3 body",author:'Blog3 author',id:3}
         ]
     )
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id!=id);
+        setBlogs(newBlogs)
+    }
+    useEffect(()=>{
+        console.log(blogs);
+    })
     return ( 
         <div className="home">
-           {blogs.map(blog=>(
-            <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-            </div>
-           ))}
+           <BlogList title='All Blogs' blogs={blogs} handleD = {handleDelete}/>
         </div>
      );
 }
