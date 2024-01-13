1a6c21ae898a33630008d7f6a00e588adea25842
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-01-13 21:38:23 +0530

    Day 15 Added Queue Implementation code -- 13/01/24

---
+++ DSA/Queues/queues_implem.js
@@ -0,0 +1,36 @@
+/*
+Queue :-- A List Like Datastructure with FIFO mechanism.
+New elements are inserted at the end of the list.
+Elements are removes from the front ie., from index 0 of the list.
+*/
+
+//Enqueue :-- Adding an element at the end
+class Queue{
+    constructor(){
+        this.dataStore = [];
+    }
+
+    enqueue(element){
+        this.dataStore.push(element);
+    }
+
+    dequeue(){
+        this.dataStore.shift();
+    }
+
+    front(){
+        return this.dataStore[0];
+    }
+
+    back(){
+        return this.dataStore[this.dataStore.length - 1]
+    }
+
+    toString(){
+        var retStr = '';
+        for(var i = 0; i<this.dataStore.length;++i){
+            retStr += this.dataStore[i]+'\n';
+        }
+        return retStr
+    }
+}
