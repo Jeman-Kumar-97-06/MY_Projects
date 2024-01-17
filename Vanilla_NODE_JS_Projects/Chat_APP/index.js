4bef329f6bd4bbd24a06c1982a68bd45ef64943c
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-01-17 12:50:08 +0530

    Day 17 Removed React Blog App to redo again --17/01/2024

--- Vanilla_NODE_JS_Projects/Chat_APP/index.js
+++ Vanilla_NODE_JS_Projects/Chat_APP/index.js
@@ -11,14 +11,19 @@ app.get('/',(req,res)=>{
     res.render('index');
 });
 
+app.get('/my-room',(req,res)=>{
+    res.render('index');
+})
+
 io.on('connection',(socket)=>{
     console.log('socket connection established');
     socket.on('chat message',(msg)=>{
         io.emit('chat message',msg);
     });
-    socket.join('My Room');
-    io.to('My Room').emit('chat message',msg);
-    
+    socket.on('chat message to my room',(msg)=>{
+        socket.join('My Room');
+        io.to('My Room').emit('chat message',msg);
+    })
 });
 
 server.listen('3000',()=>{console.log('Listening at 3000')})
