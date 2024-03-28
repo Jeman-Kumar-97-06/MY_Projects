18544ea1d9a7bb49a0aee84caf90c09da6a10ae9
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-03-28 17:01:43 +0530

    Done with Backend Code for MERN APP : WorkoutBuddy --28/03/2024

--- MERN_Stack_Apps/workout_buddy/backend/controllers/workoutController.js
+++ MERN_Stack_Apps/workout_buddy/backend/controllers/workoutController.js
@@ -6,9 +6,14 @@ const getWorkouts = async (req,res) => {
     const workouts = await Workout.find({}).sort({createdAt:-1});
     res.status(200).json(workouts);
 }
+
 //function to GET a single workout :
 const getWorkout   = async (req,res) => {
     const {id} = req.params;
+    //Seeing if the id is valid and follows the rules given by mongodb [rules for forming the id]
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"No such workout!"})
+    }
     const workout = await Workout.findById(id);
     if(!workout){
         return res.status(404).json({error:"No such workout"});
@@ -30,11 +35,35 @@ const createWorkout = async (req,res) => {
 
 
 //function to DELETE a workout :
+const deleteWorkout = async (req,res) => {
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"No SUch Workout!"})
+    }
+    const workout = await Workout.findOneAndDelete({_id:id});
+    if(!workout){
+        return res.status(400).json({error:"No Such Workout!"});
+    }
+    res.status(200).json(workout);
+}
 
 //function to UPDATE a workout :
+const updateWorkout = async (req,res) => {
+    const {id} = req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"No such Workout!"});
+    };
+    const workout = await Workout.findOneAndUpdate({_id:id},{...req.body});
+    if(!workout){
+        return res.status(400).json({error:"No Such Workout!"})
+    };
+    res.status(200).json(workout);
+}
 
 module.exports = {
     getWorkouts,
     getWorkout,
-    createWorkout
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
 }
--- MERN_Stack_Apps/workout_buddy/backend/routes/workouts.js
+++ MERN_Stack_Apps/workout_buddy/backend/routes/workouts.js
@@ -1,5 +1,9 @@
 const exp                                    = require('express');
-const {getWorkouts,getWorkout,createWorkout} = require('../controllers/workoutController')
+const {getWorkouts,
+       getWorkout,
+       createWorkout,
+       deleteWorkout,
+       updateWorkout                       } = require('../controllers/workoutController')
 const router                                 = exp.Router();
 
 //ROUTE Handler Functions :--
@@ -13,13 +17,9 @@ router.get('/:id',getWorkout)
 router.post('/',createWorkout)
 
 //DELETE a workout
-router.delete('/:id',(req,res)=>{
-    res.json({mssg:"DELETE a workout with id"})
-})
+router.delete('/:id',deleteWorkout)
 
 //UPDATE a workout
-router.patch('/:id',(req,res)=>{
-    res.json({mssg:"UPDATE a workout with id"})
-})
+router.patch('/:id',updateWorkout)
 
 module.exports = router;
