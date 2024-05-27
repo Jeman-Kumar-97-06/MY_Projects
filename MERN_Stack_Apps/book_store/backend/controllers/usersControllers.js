02c5bcfa45372816eed48e97aedbf853cd64e704
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-05-27 20:51:46 +0530

    Writing the Auth Code first --27/5/24

--- MERN_Stack_Apps/book_store/backend/controllers/usersControllers.js
+++ MERN_Stack_Apps/book_store/backend/controllers/usersControllers.js
@@ -1,3 +1,4 @@
+const BookUser = require('../models/userModel');
 //LOGIN USER :
 const loginUser = async (req,res) => {
     res.json({mssg:"LOGIN USER"});
@@ -5,7 +6,14 @@ const loginUser = async (req,res) => {
 
 //SIGNUP USER :
 const signupUser = async (req,res) => {
-    res.json({mssg:"SIGNUP USER"});
+    const {email,password}  = req.body;
+    try{
+        const user = await BookUser.signup(email,password);
+        res.status(200).json({email,user});
+    }
+    catch(error){
+        res.status(400).json({error:error.message});
+    }
 }
 
 module.exports = {loginUser,signupUser};
