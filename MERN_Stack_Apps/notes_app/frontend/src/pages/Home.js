8c675cba9fab0fcfa4c9a13522901c7757f3e2db
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-06-09 22:17:09 +0530

    Writing Frontend code for notes_app --9/6/24

---
+++ MERN_Stack_Apps/notes_app/frontend/src/pages/Home.js
@@ -0,0 +1,26 @@
+const Home = () => {
+    return (
+        <div className='home_page'>
+            <div className="note_each">
+                <h2>A Note on Evolution</h2>
+                <p>Evolution can be said to be a step by step procedure where living things’ traits and physical
+                     attributes adjust over a given long interval. The theory was first put together by Charles Darwin
+                      in 1859, he narrates the procedure of adjustments organisms undergo to survive and adapt better to their 
+                      immediate habitat. Regardless of the controversies surrounding Charles Darwin’s theory particularly on its
+                       opposing stance as regards religion, the theory has been widely accepted. The theory has provided a staple
+                        for research, findings into evolution, and with so much evidence at disposal today, acceptance will almost be 
+                        impossible (Costa 2009 p. 890). It has been shown by numerous studies that not only does evolution explain our 
+                        characteristics and behaviors today, but it is also very much happening, and humans are still evolving – at a rate 
+                        that is even faster. This then means homo sapiens in the future will most definitely have specific characteristics 
+                        as a result of evolution which will enable them to adapt better to the environment then. This paper will look at various
+                         aspects of evolution such as the theories of human evolution, the evidence available for evolution and the natural selection 
+                         concept to explain and describe the characteristics of homo sapiens in the future with facts 
+                         from the present and the past.</p>
+                <button>Update</button>
+                <button>Delete</button>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
