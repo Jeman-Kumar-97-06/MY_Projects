208ad5805ffa8f8bd3c8522d6df69f4b6daee01b
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-06-13 22:17:42 +0530

    Doing OST_IEC Project --13/6/24

--- Vanilla_NODE_JS_Projects/OST_IEC_62443/server.js
+++ Vanilla_NODE_JS_Projects/OST_IEC_62443/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app     = express();
+const questions_data = require('./data/questionare.json');
+
 
 app.set('view engine','ejs'); 
 
 app.get('/',(req,res)=>{
     res.render('index.ejs');
+})
+
+app.get('/security_assessment',(req,res)=>{
+    res.render('questionaire.ejs',{data:questions_data});
 })
 
 app.listen(3000,()=>{console.log("listening to requests at 3000")});
