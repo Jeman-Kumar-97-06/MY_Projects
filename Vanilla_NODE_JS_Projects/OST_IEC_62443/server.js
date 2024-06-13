7a704db38f58eff81ebfd2fe9129aa66eb7faafe
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-06-13 21:20:38 +0530

    Doing OST project --13/6/24

---
+++ Vanilla_NODE_JS_Projects/OST_IEC_62443/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app     = express();
+
+app.set('view engine','ejs'); 
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs');
+})
+
+app.listen(3000,()=>{console.log("listening to requests at 3000")});
