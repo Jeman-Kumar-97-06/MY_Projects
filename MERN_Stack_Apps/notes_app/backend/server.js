a8b87968274f2671f54991c02619e00d6d330df5
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-06-15 21:11:56 +0530

    Added backend code for MERN Stack notes_app --15/6/24

--- MERN_Stack_Apps/notes_app/backend/server.js
+++ MERN_Stack_Apps/notes_app/backend/server.js
@@ -1,13 +1,27 @@
-const express = require('express');
+require('dotenv').config();
+
+const express    = require('express');
+const mongoose   = require('mongoose');
+const noteRoutes = require('./routes/notes');
+
+
 
 //creating express app:
 const app     = express();
+
+//MIDDLEWARE to accept form data from client side as json. If you don't have the following line...
+//... the form data will be recognized as 'undefined'.
+app.use(express.json())
 
 //routes:
 app.get('/',(req,res)=>{
     res.json({mssg:"Welcome to the app!"});
 })
 
-//listening to requests:
-app.listen(4000,()=>{console.log('Listening to requests at 4000')});
+app.use("/notes",noteRoutes);
+
+//Connecting to db & listening to requests:
+mongoose.connect(process.env.MONG_URI).then(()=>{
+    app.listen(process.env.PORT,()=>{console.log(`Listening to requests at ${process.env.PORT}`)});
+}).catch(err=>{console.log(err)});
 
