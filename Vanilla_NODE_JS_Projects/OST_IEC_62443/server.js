2f9071be15b3877055d83194bbf16d6d43f55295
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-06-17 12:56:38 +0530

    Graph functionality for OST_IEC project --17/6/24

--- Vanilla_NODE_JS_Projects/OST_IEC_62443/server.js
+++ Vanilla_NODE_JS_Projects/OST_IEC_62443/server.js
@@ -14,9 +14,56 @@ app.get('/security_assessment',(req,res)=>{
 })
 
 app.post('/sec_assess_repl',(req,res)=>{
-    console.log(typeof req.body);
-    console.log(JSON.stringify(req.body));
-    res.render('plots.ejs',{pl:JSON.stringify(req.body)})
+    const plt  = req.body; 
+    const kys  = Object.keys(plt);
+    const resp = {low:0,medium:0,high:0,critical:0,ignore:0};
+    for (var i=0;i<kys.length;i++)
+        {
+            if(questions_data[i].factor==='Integrity'){
+                if(plt[kys[i]]=='not_implemented')
+                    {
+                        resp.high +=1;
+                    }
+                else if(plt[kys[i]]=='partially_implemented')
+                    {
+                        resp.medium += 1;
+                    }
+                else if(plt[kys[i]]=='implemented')
+                    {
+                        resp.ignore +=1;
+                    }
+            }
+            else if(questions_data[i].factor==='Availability'){
+                if(plt[kys[i]]=='not_implemented')
+                    {
+                        resp.critical +=1;
+                    }
+                else if(plt[kys[i]]=='partially_implemented')
+                    {
+                        resp.medium += 1;
+                    }
+                else if(plt[kys[i]]=='implemented')
+                    {
+                        resp.ignore +=1;
+                    }
+            }
+            else if(questions_data[i].factor==='Confidentiality'){
+                if(plt[kys[i]]=='not_implemented')
+                    {
+                        resp.medium +=1;
+                    }
+                else if(plt[kys[i]]=='partially_implemented')
+                    {
+                        resp.low += 1;
+                    }
+                else if(plt[kys[i]]=='implemented')
+                    {
+                        resp.ignore +=1;
+                    }
+            }
+
+        }
+    res.render('plots.ejs',{total:questions_data.length,low:resp.low,medium:resp.medium,high:resp.high,critical:resp.critical});
 })
 
 app.listen(3000,()=>{console.log("listening to requests at 3000")});
