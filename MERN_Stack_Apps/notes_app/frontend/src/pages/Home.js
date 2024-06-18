738b8a440b1064435c951e83c4142ee3088dc4a0
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-06-18 20:57:28 +0530

    Working on frontend --18/6/24

---
+++ MERN_Stack_Apps/notes_app/frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import {useEffect,useState} from 'react';
+const Home = () => {
+    useEffect(()=>{
+        
+    })
+    return (
+        <div className = 'home'>
+            <h1>Home</h1>
+        </div>
+    )
+};
+export default Home;
