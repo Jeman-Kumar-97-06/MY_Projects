c34c897c7a3187f366284e9f7c96cddc309b5bc1
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-07-20 21:44:25 +0530

    Almost Done with Backend code for ToDoList app --20/7/24

--- MERN_Stack_Apps/To_Do_List/backend/models/listModel.js
+++ MERN_Stack_Apps/To_Do_List/backend/models/listModel.js
@@ -1,2 +1,9 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const mongoose   = require('mongoose');
+const Schema     = mongoose.Schema;
+
+const listSchema = new Schema({
+    title : {type:String,required:true},
+    list  : [{type:String,required:true}]
+});
+
+module.exports = mongoose.model('ToDoListTest',listSchema);
