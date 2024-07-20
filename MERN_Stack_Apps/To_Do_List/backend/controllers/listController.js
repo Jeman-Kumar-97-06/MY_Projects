13c318364a153abd21736a47f5e95ebc7f76042d
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-07-20 21:36:28 +0530

    Almost Done with Backend code for ToDoList app --20/7/24

--- MERN_Stack_Apps/To_Do_List/backend/controllers/listController.js
+++ MERN_Stack_Apps/To_Do_List/backend/controllers/listController.js
@@ -24,7 +24,45 @@ const getList = async (req,res) => {
 
 //controller to create a new list
 const createList = async (req,res) => {
-    const {title,load,reps} = 
+    const {title,load,reps} = req.body;
+    try
+    {
+        const list_yo = await List.create({title,load,reps});
+        res.status(200).json(list_yo);
+    }
+    catch(err)
+    {
+        res.status(400).json({error:err.message});
+    }   
+}
+
+//controller to delete a list
+const deleteList = async (req,res) => {
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(404).json({error:"Invalid ID!"});
+    }
+    const list_yo = await List.findOneAndDelete({_id:id});
+    if(!list_yo)
+    {
+        return res.status(404).json({error:"No List found with the specified ID!"})
+    }
+    res.status(200).json(list_yo);
+}
+
+const updateList = async (req,res) => {
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id))
+    {
+        return res.status(404).json({error:"Invalid ID!"});
+    }
+    const list_yo = await List.findOneAndUpdate({_id:id},{...req.body});
+    if(!list_yo)
+    {
+        return res.status(404).json({error:"No List found with the specified ID!"});
+    }
+    res.status(200).json(list_yo);
 }
 
 module.exports = {createList,getLists,getList,updateList,deleteList};
