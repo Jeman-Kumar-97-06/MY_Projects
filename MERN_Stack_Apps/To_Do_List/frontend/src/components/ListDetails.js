8f77bb6f40ee06c5514a941f1997831a7645a739
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-07-21 21:09:53 +0530

    Writing Frontend code for ToDoList App;

--- MERN_Stack_Apps/To_Do_List/frontend/src/components/ListDetails.js
+++ MERN_Stack_Apps/To_Do_List/frontend/src/components/ListDetails.js
@@ -2,13 +2,16 @@ const ListDetails = ({l_e}) => {
     return (
         <div className="each_lis">
             <h2>{l_e.title}</h2>
-            <ul>
+            <form>
             {
               l_e.list.map((l_e_l)=>(
-                    <li>{l_e_l}</li>
+                <div>
+                    <input type="checkbox" id={l_e_l} />
+                    <label for={l_e_l}>{l_e_l}</label>
+                </div>
                 ))
-            }
-            </ul>
+            } 
+            </form>
             <button><a href="#">Delete</a></button>
             <button><a href="#">Save</a></button>
         </div>
