971c845984a64125547380f7ed6beeb4fb7e9c99
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-07-22 22:07:57 +0530

    Made changes to frontendcode

--- MERN_Stack_Apps/To_Do_List/frontend/src/components/ListDetails.js
+++ MERN_Stack_Apps/To_Do_List/frontend/src/components/ListDetails.js
@@ -1,19 +1,28 @@
+import { useState } from "react";
 const ListDetails = ({l_e}) => {
+
+    const [list_it,setList_it] = useState(l_e.list);
+
+    const updateList = (e) => {
+        const new_arr = list_it.splice(list_it.indexOf(e.target.id)-1,1);
+        console.log(new_arr)
+    }
+
     return (
         <div className="each_lis">
             <h2>{l_e.title}</h2>
             <form>
-            {
-              l_e.list.map((l_e_l)=>(
-                <div>
-                    <input type="checkbox" id={l_e_l} />
-                    <label for={l_e_l}>{l_e_l}</label>
-                </div>
-                ))
-            } 
+                {
+                    l_e.list.map((l_e_l)=>(
+                        <div>
+                            <input type="checkbox" id={l_e_l} onClick={e=>updateList(e)}/>
+                            <label htmlFor={l_e_l}>{l_e_l}</label>
+                        </div>
+                                          ))
+                } 
             </form>
             <button><a href="#">Delete</a></button>
-            <button><a href="#">Save</a></button>
+            
         </div>
     )
 }
