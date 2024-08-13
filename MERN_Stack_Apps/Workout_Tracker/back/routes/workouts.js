e854b638b90b043f53a32ca2298ae9bfe996abdb
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-08-13 17:59:44 +0530

    Completed Backend API code for Workout_Tracker

--- MERN_Stack_Apps/Workout_Tracker/back/routes/workouts.js
+++ MERN_Stack_Apps/Workout_Tracker/back/routes/workouts.js
@@ -1,33 +1,17 @@
 const express = require('express');
 const Workout = require('../models/workoutModel');
+const { createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout } = require('../controllers/workoutControllers');
 const router = express.Router();
 
 
-router.get('/',(req,res)=>{
-    res.json({mssg:'GET All workouts'})
-})
+router.get('/',getWorkouts);
 
-router.get('/:id',(req,res)=>{
-    res.json({mssg:"GET A single workout"});
-})
+router.get('/:id',getWorkout);
 
-router.post('/',async (req,res)=>{
-    const {title,load,reps} = req.body;
-    try {
-        const new_workout = await Workout.create({title,load,reps});
-        res.status(200).json(new_workout);
-    } catch(error) {
-        res.status(400).json({error:error.message});
-    }
-    res.json({mssg:"POST A new workout"})
-})
+router.post('/',createWorkout);
 
-router.delete('/:id',(req,res)=>{
-    res.json({mssg:"DELETE A workout"})
-})
+router.delete('/:id',deleteWorkout);
 
-router.patch('/:id',(req,res)=>{
-    res.json({mssg:"Update a workout"})
-})
+router.patch('/:id',updateWorkout);
 
 module.exports = router;
