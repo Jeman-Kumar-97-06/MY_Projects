7b5205046fc8c86a2c0876c3eb5a7c585088a73f
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-08-30 20:29:54 +0530

    Made changes to frontend code of QuizGame

--- MERN_Stack_Apps/QuizGame/frontend/src/pages/Home.js
+++ MERN_Stack_Apps/QuizGame/frontend/src/pages/Home.js
@@ -4,6 +4,23 @@ const Home = () => {
 
     const [qs,setQs] = useState(null);
 
+    const selectedOptions = {};
+
+    const handleOptionSelect = (e,corr) => {
+        selectedOptions[e.target.name] = [e.target.value,corr];
+    }
+
+    const handleAnswersSubmit = (e) => {
+        e.preventDefault();
+        let score = 0;
+        Object.keys(selectedOptions).forEach(k=>{
+            if (selectedOptions[k][0]===selectedOptions[k][1]){
+                score+=1
+            }
+        })
+        console.log(score);
+    }
+
     useEffect(()=>{
         const fetchQuestions = async () => {
             const resp = await fetch('http://localhost:4000/api/questions/');
@@ -15,11 +32,22 @@ const Home = () => {
 
     return (
         <div className='home_page'>
-            {qs && qs.map(q=>(
-                <div className='each_q' key={q._id}>
-                    <h1>{qs.indexOf(q)+1}    {q.question}</h1>
-                </div>
-            ))}
+            <form className='questions' onSubmit={handleAnswersSubmit}>
+                {/* Rendering each question */}
+                {qs && qs.map(q=>(
+                    <div className='each_q' key={q._id}>
+                        <h4>{q.question}</h4>
+                        {/* Rendering each option */}
+                        {q.all_options.map(option=>(
+                            <div className='each_option'>
+                                <input type="radio" id={option} name={`question${qs.indexOf(q)}`} value={option} onClick={e=>handleOptionSelect(e,q.correct_option)}/>
+                                <label htmlFor={option}>{option}</label>
+                            </div>
+                                                    ))}
+                    </div>
+                                ))}
+                <button type='submit'>Submit</button>
+            </form>
         </div>
     )
 }
