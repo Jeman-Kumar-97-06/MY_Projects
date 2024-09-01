afcb6b57f5e0f4b49e1779bd3b6a15e4523a7daa
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-01 22:00:09 +0530

    Changes made in frontend code of QuizGame

---
+++ MERN_Stack_Apps/QuizGame/frontend/src/components/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <div className="nav_bar_comp">
+            <div className="nav1_2">
+                <a href='#'>Computer_Quiz</a>
+            </div>
+            <div className="nav2_2">
+                <a href='#'>High Score</a>
+                <a href='#'>Login</a>
+                <a href='#'>Sign Up</a>
+                <a href='#'>Log Out</a>
+            </div>
+        </div>
+    )
+};
+
+export default Navbar;
