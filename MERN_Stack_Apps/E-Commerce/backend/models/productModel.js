62980e0677d774bd966d1008b08e0e923491727f
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-03 15:33:53 +0530

    Made changes to E-Commerce Website

--- MERN_Stack_Apps/E-Commerce/backend/models/productModel.js
+++ MERN_Stack_Apps/E-Commerce/backend/models/productModel.js
@@ -3,9 +3,11 @@ const Schema   = mongoose.Schema;
 
 const productSchema = new Schema({
     prod_name  : {type:String,required:true},
-    prod_desc  : {type:String,required:true},
+    prod_rtng  : {type:String,required:true},
     prod_price : {type:String,required:true},
-    prod_image : {type:String,required:true}
+    prod_image1 : {type:String,required:true},
+    prod_image2 : {type:String,required:true},
+    prod_image3 : {type:String,required:true}
 },{timestamps:true});
 
 module.exports = mongoose.model('productsfinal',productSchema);
