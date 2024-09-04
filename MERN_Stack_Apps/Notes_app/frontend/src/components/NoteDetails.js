f9a46206de0c25dc9b17ebe0038000b6c0d2cf33
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-04 21:59:44 +0530

    changes made to notes app;

--- MERN_Stack_Apps/Notes_app/frontend/src/components/NoteDetails.js
+++ MERN_Stack_Apps/Notes_app/frontend/src/components/NoteDetails.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import { useNoteContext } from "../hooks/useNoteContext";
 
 const NoteDetails = ({nt}) => {
+
+    const [newtitle,setNewtitle] = useState(nt.title);
+    const [newbody,setNewbody]   = useState(nt.body);
+
     const {dispatch} = useNoteContext();
     const handleDel = async () => {
         const resp = await fetch('/api/notes/'+nt._id,{method:'DELETE'});
@@ -8,13 +13,21 @@ const NoteDetails = ({nt}) => {
         if(resp.ok){
             dispatch({type:"DELETE_NOTE",payload:json})
         }
+    };
+
+
+    const handleUpdate  = () => {
+        console.log(newtitle);
+        console.log(newbody);
     }
+
     return (
-        <div class='nt_det'>
-            <h4>{nt.title}</h4>
+        <div className='nt_det'>
+            <h4 autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" contentEditable='true' suppressContentEditableWarning="true" onInput={(e)=>{document.querySelector(`#updtebtn${nt._id}`).style.display='inline-block';setNewtitle(e.target.value)}} value={newtitle}>{newtitle}</h4>
             <hr/>
-            <p>{nt.body}</p>
+            <p autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" contentEditable='true' suppressContentEditableWarning="true" onInput={(e)=>{document.querySelector(`#updtebtn${nt._id}`).style.display='inline-block';setNewbody(e.target.value)}} value={newbody}>{newbody}</p>
             <button onClick={handleDel} className="del_note_btn">Delete</button>
+            <button id={`updtebtn${nt._id}`} style={{display:'none'}} onClick={handleUpdate}>Update</button>
         </div>
     )
 }
