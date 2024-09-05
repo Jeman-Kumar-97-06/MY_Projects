eea70375994aeb59165e8d96f08346542672f3ba
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-05 23:17:18 +0530

    Made changes to Note app

--- MERN_Stack_Apps/Notes_app/frontend/src/components/NoteDetails.js
+++ MERN_Stack_Apps/Notes_app/frontend/src/components/NoteDetails.js
@@ -3,32 +3,10 @@ import { useNoteContext } from "../hooks/useNoteContext";
 
 const NoteDetails = ({nt}) => {
 
-    // Get the modal
-    var modal = document.getElementById("updateModal");
+    const [title,setTitle] = useState(nt.title);
+    const [body,setBody]   = useState(nt.body);
 
-    // Get the button that opens the modal
-    var btn = document.getElementById("updateBtn");
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
-
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    } 
-
+    const [error,setError] = useState(null);
 
     const {dispatch} = useNoteContext();
     const handleDel = async () => {
@@ -40,23 +18,28 @@ const NoteDetails = ({nt}) => {
     };
 
 
-    const handleUpdate  = () => {
-        console.log('clicked');
+    const handleUpdate  = async () => {
+        const update_n = {title,body};
+        const resp     = await fetch(`/api/notes/${nt._id}`,{method:"PATCH",body:JSON.stringify(update_n),headers:{'Content-Type':'application/json'}});
+        const json     = await resp.json();
+        if(!resp.ok){
+            setError(json.error);
+        }
+        if(resp.ok){
+            setError(null);
+            console.log('note updated!')
+            setTitle('');
+            setBody('');
+            dispatch({type:'CREATE_NOTE',payload:json});
+        }
     }
 
     return (
         <div className='nt_det'>
-            <h4>{nt.title}</h4>
-            <hr/>
-            <p>{nt.body}</p>
+            <input type='text' onChange={e=>{setTitle(e.target.value)}} value={title}/><br/>
+            <textarea onChange={e=>{setBody(e.target.value)}} value={body}/><br/>
             <button onClick={handleDel} className="del_note_btn">Delete</button>
             <button id='updateBtn' onClick={handleUpdate}>Update</button>
-            <div id="updateModal" className="modal">
-                <div className="modal-content">
-                    <span className="close">&times;</span>
-                    <p>Some text in the Modal..</p>
-                </div>
-            </div>
         </div>
     )
 }
