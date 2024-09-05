994623ec84278a1ad2ea8ac79c55ebfe565cf902
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-05 22:19:42 +0530

    Made changes to Note app

--- MERN_Stack_Apps/Notes_app/frontend/src/components/NoteDetails.js
+++ MERN_Stack_Apps/Notes_app/frontend/src/components/NoteDetails.js
@@ -3,8 +3,32 @@ import { useNoteContext } from "../hooks/useNoteContext";
 
 const NoteDetails = ({nt}) => {
 
-    const [newtitle,setNewtitle] = useState(nt.title);
-    const [newbody,setNewbody]   = useState(nt.body);
+    // Get the modal
+    var modal = document.getElementById("updateModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("updateBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    } 
+
 
     const {dispatch} = useNoteContext();
     const handleDel = async () => {
@@ -17,17 +41,22 @@ const NoteDetails = ({nt}) => {
 
 
     const handleUpdate  = () => {
-        console.log(newtitle);
-        console.log(newbody);
+        console.log('clicked');
     }
 
     return (
         <div className='nt_det'>
-            <h4 autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" contentEditable='true' suppressContentEditableWarning="true" onInput={(e)=>{document.querySelector(`#updtebtn${nt._id}`).style.display='inline-block';setNewtitle(e.target.value)}} value={newtitle}>{newtitle}</h4>
+            <h4>{nt.title}</h4>
             <hr/>
-            <p autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false" contentEditable='true' suppressContentEditableWarning="true" onInput={(e)=>{document.querySelector(`#updtebtn${nt._id}`).style.display='inline-block';setNewbody(e.target.value)}} value={newbody}>{newbody}</p>
+            <p>{nt.body}</p>
             <button onClick={handleDel} className="del_note_btn">Delete</button>
-            <button id={`updtebtn${nt._id}`} style={{display:'none'}} onClick={handleUpdate}>Update</button>
+            <button id='updateBtn' onClick={handleUpdate}>Update</button>
+            <div id="updateModal" className="modal">
+                <div className="modal-content">
+                    <span className="close">&times;</span>
+                    <p>Some text in the Modal..</p>
+                </div>
+            </div>
         </div>
     )
 }
