c875114dfbf47c8aae9a8f71ba474bb205fcebdb
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-05 23:22:03 +0530

    Made changes to Note app

--- MERN_Stack_Apps/Notes_app/frontend/src/components/NoteDetails.js
+++ MERN_Stack_Apps/Notes_app/frontend/src/components/NoteDetails.js
@@ -22,24 +22,25 @@ const NoteDetails = ({nt}) => {
         const update_n = {title,body};
         const resp     = await fetch(`/api/notes/${nt._id}`,{method:"PATCH",body:JSON.stringify(update_n),headers:{'Content-Type':'application/json'}});
         const json     = await resp.json();
-        if(!resp.ok){
-            setError(json.error);
-        }
-        if(resp.ok){
-            setError(null);
-            console.log('note updated!')
-            setTitle('');
-            setBody('');
-            dispatch({type:'CREATE_NOTE',payload:json});
-        }
+        console.log(json);
+        // if(!resp.ok){
+        //     setError(json.error);
+        // }
+        // if(resp.ok){
+        //     setError(null);
+        //     console.log('note updated!')
+        //     setTitle('');
+        //     setBody(nt.body);
+        //     dispatch({type:'CREATE_NOTE',payload:json});
+        // }
     }
 
     return (
         <div className='nt_det'>
-            <input type='text' onChange={e=>{setTitle(e.target.value)}} value={title}/><br/>
-            <textarea onChange={e=>{setBody(e.target.value)}} value={body}/><br/>
+            <input type='text' onChange={e=>{document.querySelector('#updateBtn'+nt._id).style.display='inline-block';setTitle(e.target.value)}} value={title}/><br/>
+            <textarea onChange={e=>{document.querySelector('#updateBtn'+nt._id).style.display='inline-block';setBody(e.target.value)}} value={body}/><br/>
             <button onClick={handleDel} className="del_note_btn">Delete</button>
-            <button id='updateBtn' onClick={handleUpdate}>Update</button>
+            <button id={`updateBtn${nt._id}`}onClick={handleUpdate} style={{display:'none'}}>Update</button>
         </div>
     )
 }
