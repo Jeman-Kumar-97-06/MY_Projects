e5934452a9e4dc23171ecfbd5b7e4dab87c923a5
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-08 22:16:08 +0530

    Making changes to WorkoutTracker

---
+++ MERN_Stack_Apps/Workout_Tracker/backend/routes/workouts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router  = express.Router();
+
+router.get('/',);
+
+router.get('/:id');
+
+router.post('/');
+
+router.delete('/:id');
+
+router.patch('/:id');
+
+module.exports = router;
--- MERN_Stack_Apps/Workout_Tracker/backend/server.js
+++ MERN_Stack_Apps/Workout_Tracker/backend/server.js
@@ -1,11 +1,24 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
 const express = require('express');
 
 //Creating express app.
 const app     = express();
 
+//middleware to receive request body as json and not as 'undefined':
+app.use(express.json());
+
+const workoutRoutes = require('./routes/workouts');
+
+//Route Handlers:
+app.use('/api/workouts', workoutRoutes);
 
 
 //Listening for requests.
-app.listen(4000,()=>{console.log(
-    'listening at 4000'
-)})
+mongoose.connect(process.env.MONGOURL).then(()=>{
+    app.listen(process.env.PORT,()=>{console.log(
+        `Listening at ${process.env.PORT}`
+    )})
+}).catch(err=>{console.log(err)})
