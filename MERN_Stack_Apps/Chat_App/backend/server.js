b80ba8b4e6666c02379872a23672b69938eae4df
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-26 00:08:28 +0530

    Working on socket.io ChatApp

---
+++ MERN_Stack_Apps/Chat_App/backend/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const {createServer} = require('node:http');
+const {join} = require('node:path');
+const {Server} = require('socket.io');
+
+const app = express();
+
+const server = createServer(app);
+
+const io = new Server(server);
+
+app.get('/',(req,res)=>{
+    res.sendFile(join(__dirname,'index.html'))
+})
+
+io.on('connection',(socket)=>{
+    console.log('a user connected');
+    console.log(socket)
+})
+
+server.listen(3000,()=>{
+    console.log('server running at 3000');
+})
