e808619dbaf89e8d945727a19b7fbbdb0b1a4d99
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-09-26 20:44:37 +0530

    Worked on Chat App from Socket.io Tutorial

--- MERN_Stack_Apps/Chat_App/backend/server.js
+++ MERN_Stack_Apps/Chat_App/backend/server.js
@@ -7,6 +7,8 @@ const app = express();
 
 const server = createServer(app);
 
+//Initializing new instance of socket.io
+//We pass 'server'as argument and allow io to check/listen to events from sockets.
 const io = new Server(server);
 
 app.get('/',(req,res)=>{
@@ -14,9 +16,16 @@ app.get('/',(req,res)=>{
 })
 
 io.on('connection',(socket)=>{
-    console.log('a user connected');
-    console.log(socket)
+    socket.on('chat msg',(msg)=>{
+        io.emit('chat msg',msg);
+    })
+    //Every time client refreshes browser/app the 'disconnect' event will be triggered
+    socket.on('disconnect',()=>{
+        console.log('a user disconnected')
+    })
 })
+
+
 
 server.listen(3000,()=>{
     console.log('server running at 3000');
