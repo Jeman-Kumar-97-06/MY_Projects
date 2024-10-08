822fa05807bbb96002f2ced1a6a9e52068e5aaaf
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-10-08 15:11:45 +0530

    Almost Completed E-Commerce App

--- src/pages/CartDets.js
+++ src/pages/CartDets.js
@@ -10,7 +10,6 @@ const CartDets = () => {
         const fetchCart = async () => {
             const resp  = await fetch('/api/carts',{headers:{"Authorization":`Bearer ${user.token}`}});
             const cart  = await resp.json();
-            console.log(cart.prices)
             if (resp.ok) {
                 setCartitems(cart.cart[0].products)
                 setPrices(cart.prices)
@@ -22,6 +21,7 @@ const CartDets = () => {
     },[user])
 
     return(
+        <div className="cart_page">
         <table className='cart_page'>
             <tr>
                 <th>Product Name</th>
@@ -30,12 +30,18 @@ const CartDets = () => {
             </tr>
             {cartitems && cartitems.map(ci=>(
                 <tr>
+                    {/* The following is book names */}
                     <td>{Object.keys(ci)[0]}</td>
-                    <td>{Object.values(ci)[0]}</td>
+                    {/* The following is books quantity */}
+                    <td><input type='number' value={Object.values(ci)[0]}/></td>
+                    {/* The following is book prices */}
                     <td>{Number(prices[Object.keys(ci)[0]])*Object.values(ci)[0]}</td>
                 </tr>
             ))}
         </table>
+        <button className="update_cart_btn">Update Cart</button>
+        <button className="update_cart_btn">Checkout</button>
+        </div>
     )
 };
 
