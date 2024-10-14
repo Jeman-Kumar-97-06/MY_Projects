7116f119110b948e30343cd81828f8229df2bd55
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-10-14 22:38:13 +0530

    Made changes to Wallpaper App

--- MERN_Stack_Apps/Wallpaper_App/backend/index.js
+++ MERN_Stack_Apps/Wallpaper_App/backend/index.js
@@ -8,7 +8,8 @@ const app      = express();
 app.use(cors());
 app.use(express.json());
 
-app.use()
+app.use('/api/wallpapers',wRt);
+app.use('/api/user',uRt);
 
 mongoose.connect(process.env.MONGOURL).then(()=>{
     app.listen(process.env.PORT,()=>{console.log(`Connected to DB and listening at ${process.env.PORT}`)});
