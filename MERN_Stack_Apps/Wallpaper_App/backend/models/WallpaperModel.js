c87c9749f2e5157881fafd6672e1aa43d74b6823
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-10-15 11:11:08 +0530

    Deleted some un-necessary files

--- MERN_Stack_Apps/Wallpaper_App/backend/models/WallpaperModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('monoose');
-const Schema   = mongoose.Schema;
-
-const wallpaperSchema = new Schema({
-    path : {type:String,required:true},
-    user_id : {type:String,required:true}
-});
-
-module.exports = mongoose.model('wallpaper',wallpaperSchema);
