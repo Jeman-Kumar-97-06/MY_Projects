967726e22f1702220f1695d3341d5f613b12ed8b
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-10-27 22:48:24 +0530

    Made changes to E-Commerce

---
+++ src/components/CartItem.js
@@ -0,0 +1,19 @@
+import { useState } from "react"
+const CartItem = ({ci,prices}) => {
+    const [quantity,setQuantity]   = useState(Object.values(ci)[0]);
+    console.log(ci)
+    return (
+        <tr>
+                    {/* The following is book names */}
+                    <td>{Object.keys(ci)[0]}</td>
+                    {/* The following is books quantity */}
+                    <td><input type='number' value={quantity} onChange={(e)=>{setQuantity(e.target.value)}}/></td>
+                    {/* The following is book prices */}
+                    <td>{Number(prices[Object.keys(ci)[0]])*Object.values(ci)[0]}</td>
+                    {/* The following is just a DELET btn */}
+                    <td><button>Delete</button></td>
+        </tr>
+    )
+}
+
+export default CartItem;
