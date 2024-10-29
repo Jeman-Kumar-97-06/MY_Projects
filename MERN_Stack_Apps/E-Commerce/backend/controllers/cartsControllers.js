1a266a5400531653243ddd99a8223072d6627428
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-10-29 18:56:55 +0530

    Made changes to e-commerce

--- MERN_Stack_Apps/E-Commerce/backend/controllers/cartsControllers.js
+++ MERN_Stack_Apps/E-Commerce/backend/controllers/cartsControllers.js
@@ -24,30 +24,7 @@ const getCart = async (req,res) => {
 const createCart = async (req,res) => {
     const {pt} = req.body
     console.log(pt)
-    const m_key = Object.keys(pt)[0]
-    const exists = await Carts.findOne({user_id:req.user._id});
-    if (exists){
-        //Array of all products Ex:[{Dune:1},{Ender's Game:3}]
-        const old_prod = exists.products;
-        let found    = false;
-        for (let i = 0; i < old_prod.length; i++) {
-            const element = old_prod[i];
-            const key     = Object.keys(element)[0]
-            if (m_key == key){
-                old_prod[i][m_key] += pt[m_key];
-                found = true;
-            }
-        }
-        if (!found){
-            old_prod.push(pt)
-        }
-        const cart_to_patch = await Carts.findOneAndUpdate({user_id:req.user._id},{products:old_prod});
-        res.status(200).json(cart_to_patch);
-    }
-    else {
-        const new_cart = await Carts.create({products:[pt],user_id:req.user._id})
-        res.status(200).json(new_cart);
-    }
+    
 }
 
 module.exports = {getCart,createCart};
