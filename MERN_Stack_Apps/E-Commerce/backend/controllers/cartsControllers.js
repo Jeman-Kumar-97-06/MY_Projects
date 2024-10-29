5e498ef501838a3b875934e0b8e5e245ddf408e0
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-10-29 22:48:27 +0530

    Made changes to E-Commerce

--- MERN_Stack_Apps/E-Commerce/backend/controllers/cartsControllers.js
+++ MERN_Stack_Apps/E-Commerce/backend/controllers/cartsControllers.js
@@ -10,14 +10,8 @@ const getCart = async (req,res) => {
     if (!cart){
         return res.status(404).json({error:"No Cart with the give ID!"});
     }
-    const all_prods = cart[0].products;
-    const prices    = {};
-    for (let i = 0; i < all_prods.length; i++) {
-        const element = Object.keys(all_prods[i])[0];
-        const price   = await Product.findOne({title:element}).select("price");
-        prices[element] = price.price;
-    }
-    res.status(200).json({cart,prices})
+    const resps = cart[0].products
+    res.status(200).json({resps})
 }
 
 //Add a Product to the user's cart:--
