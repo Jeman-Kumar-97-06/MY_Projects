582e0033b5dc50a049a114f29197101dad19f910
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-10-30 17:09:01 +0530

    Made changes to E-Commerce

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,18 +1,31 @@
 import { useState } from "react"
-const CartItem = ({ci,prices}) => {
-    const [quantity,setQuantity]   = useState(Object.values(ci)[0]);
+import { useAuthContext } from "../hooks/useAuthContext";
+const CartItem = ({ci}) => {
+    //console.log(ci)
+    const {user}                   = useAuthContext();
+    const [quantity,setQuantity]   = useState(ci.quantity);
+
+    const updateCart = async () => {
+        ci.quantity = quantity;
+        const resp = await fetch('/api/carts',{method:'PATCH',body:JSON.stringify(ci),headers:{"Content-Type":'application/json',"Authorization":`Bearer ${user.token}`}});
+    }
+
+    const deleteFromCart = () => {
+
+    }
+
     return (
         <tr>
                     {/* The following is book names */}
-                    <td>{Object.keys(ci)[0]}</td>
+                    <td>{ci.prod_name}</td>
                     {/* The following is books quantity */}
                     <td><input type='number' value={quantity} onChange={(e)=>{setQuantity(e.target.value)}}/></td>
                     {/* The following is book prices */}
-                    <td>{Number(prices[Object.keys(ci)[0]])*Object.values(ci)[0]}</td>
+                    <td>{Number(ci.price)*Number(quantity)}</td>
                     {/* The following is just a Update btn */}
-                    <td><button>Update</button></td>
+                    <td><button onClick={updateCart}>Update</button></td>
                     {/* The following is just a DELET btn */}
-                    <td><button>Delete</button></td>
+                    <td><button onClick={deleteFromCart}>Delete</button></td>
         </tr>
     )
 }
