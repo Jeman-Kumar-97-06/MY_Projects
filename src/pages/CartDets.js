2a36530d920f43e8e295478b35b337219f5078b1
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-10-31 23:22:02 +0530

    made changes to Cart Page

--- src/pages/CartDets.js
+++ src/pages/CartDets.js
@@ -1,23 +1,22 @@
-
 import CartItem from "../components/CartItem";
 import { useAuthContext } from "../hooks/useAuthContext";
-import { useEffect, useState } from "react";
+import { useCartContext } from "../hooks/useCartContext";
+import { useEffect } from "react";
 const CartDets = () => {
     const {user} = useAuthContext();
-    const [cartitems,setCartitems] = useState(null);
-
+    const {cart_items,dispatch}  = useCartContext();
     useEffect(()=>{
         const fetchCart = async () => {
             const resp  = await fetch('/api/carts',{headers:{"Authorization":`Bearer ${user.token}`}});
             const cart  = await resp.json();
             if (resp.ok) {
-                setCartitems(cart.resps)
+                dispatch({type:"SET_CART_ITEMS",payload:cart.resps});
             }
         }
         if (user) {
             fetchCart();
         }
-    },[user])
+    },[user,dispatch])
 
     return(
         <div className="cart_page">
@@ -29,13 +28,14 @@ const CartDets = () => {
                 <th></th>
                 <th></th>
             </tr>
-            {cartitems && cartitems.map(ci=>(
+            {cart_items && cart_items.map(ci=>(
                 <CartItem key={ci.prod_name} ci={ci}/>
             ))}
             <tr>
                 <td>Total</td>
-                <td>Tot Quan</td>
-                <td>Tot Price</td>
+                <td>{cart_items && cart_items.reduce((total,itm)=>total+itm.quantity,0)}</td>
+                <td>{cart_items && cart_items.reduce((total,itm)=>total+Number(itm.price)*Number(itm.quantity),0)}</td>
+                <td><button>Pay</button></td>
             </tr>
         </table>
         </div>
