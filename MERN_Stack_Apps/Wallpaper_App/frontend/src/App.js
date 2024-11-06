abcc9a6334c9a786aa34919136bdb935b3552126
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-11-06 22:08:47 +0530

    Made changes to Frontend Code

--- MERN_Stack_Apps/Wallpaper_App/frontend/src/App.js
+++ MERN_Stack_Apps/Wallpaper_App/frontend/src/App.js
@@ -3,7 +3,14 @@ import {BrowserRouter,Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={user ? <Home/> : <Navigate to = '/login'/>}/>
+          <Route exact path='/cart' element = {user ? <CartDets/> : <Navigate to = '/login'/>}/>
+          <Route exact path='/login' element={!user ? <Login/> : <Navigate to = '/'/>}/>
+          <Route exact path='/signup' element={!user ? <Signup/> : <Navigate to = '/'/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
