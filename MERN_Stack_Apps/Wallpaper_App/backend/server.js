8ab7058cbdf22529b3e9d0c5067cbd167fbce5f3
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-11-08 21:58:09 +0530

    Added Upload Pics Functionality

--- MERN_Stack_Apps/Wallpaper_App/backend/server.js
+++ MERN_Stack_Apps/Wallpaper_App/backend/server.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express  = require('express');
 const mongoose = require('mongoose');
 const cors     = require('cors');
+const multer   = require('multer');
 
 const wallRts  = require('./routes/walls');
 const userRts  = require('./routes/users');
+const { db } = require('./models/wallModel');
 
 const app      = express();
 
@@ -13,6 +15,21 @@ app.use(express.json());
 
 //Using wallRts at '/api/walls'
 app.use('/api/walls',wallRts);
+
+//Recieving Requests to save a uploaded wallpaper:
+const storage = multer.diskStorage(
+    {
+        destination : function(req,file,cb) { return cb(null,'./uploads') },
+        filename    : function(req,file,cb) { return cb(null,`${Date.now()}-${file.originalname}`) }
+    }
+)
+
+const upload = multer({storage:storage});
+
+app.post('/api/upload_wall',upload.single('profile_pic'),(req,res)=>{
+    console.log(req.file.path);
+    return res.redirect('/');
+})
 
 //Using userRts at '/api/users'
 app.use('/api/users',userRts);
