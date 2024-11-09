d3757f0c6e8562a44e25dc3672d4a94034eb92df
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-11-09 23:10:49 +0530

    Made Changes

--- MERN_Stack_Apps/Wallpaper_App/backend/routes/walls.js
+++ MERN_Stack_Apps/Wallpaper_App/backend/routes/walls.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer   = require('multer');
-
+import fetch,{fileFromSync} from 'node-fetch';
 const router  = express.Router();
 
 const {
--- MERN_Stack_Apps/Wallpaper_App/frontend/src/components/Topform.js
+++ MERN_Stack_Apps/Wallpaper_App/frontend/src/components/Topform.js
@@ -1,4 +1,22 @@
+import {useState} from 'react';
+import {useWallsContext} from '../hooks/useWallsContext';
+import {useAuthContext} from '../hooks/useAuthContext';
+
 const Topform = () => {
+    const {dispatch}           = useWallsContext();
+    const {user}               = useAuthContext();
+    const [newwall,setNewwall] = useState('');
+    const [error,setError]     = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!user) {
+            setError('You must be Logged In!');
+            return;
+        }
+        const new_file = fileInput.files[0]
+    }
+
     return (
         <form action="/api/walls" method="POST" enctype="multipart/form-data">
             <h4>Upload Wallpaper:</h4>
