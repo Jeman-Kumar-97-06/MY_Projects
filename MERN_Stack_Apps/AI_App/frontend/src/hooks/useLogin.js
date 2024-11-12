0f2d7c7d064fb8106cd97341f5eb4d109f7783a7
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-11-12 22:00:07 +0530

    Made Some Corrections

--- MERN_Stack_Apps/AI_App/frontend/src/hooks/useLogin.js
+++ MERN_Stack_Apps/AI_App/frontend/src/hooks/useLogin.js
@@ -9,7 +9,9 @@ export const useLogin = () => {
     const login = async (email,password) => {
         setIsloading(true);
         setError(null);
+        console.log("DONE")
         const response = await fetch('/api/users/login',{method:"POST",headers:{'Content-Type':'application/json'},body:JSON.stringify({email,password})});
+        console.log(response)
         const json     = await response.json();
         if (!response.ok){
             setIsloading(false);
