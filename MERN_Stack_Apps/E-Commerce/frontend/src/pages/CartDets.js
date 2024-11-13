6761dc7b8aeb345f7af840455b54304228c2379a
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-11-14 00:41:51 +0530

    Made Some Changes

--- MERN_Stack_Apps/E-Commerce/frontend/src/pages/CartDets.js
+++ MERN_Stack_Apps/E-Commerce/frontend/src/pages/CartDets.js
@@ -31,13 +31,23 @@ const CartDets = () => {
             {cart_items && cart_items.map(ci=>(
                 <CartItem key={ci.prod_name} ci={ci}/>
             ))}
-            <tr>
-                <td>Total</td>
-                <td>{cart_items && cart_items.reduce((total,itm)=>total+itm.quantity,0)}</td>
-                <td>{cart_items && cart_items.reduce((total,itm)=>total+Number(itm.price)*Number(itm.quantity),0)}</td>
-                <td><button>Pay</button></td>
-            </tr>
         </table>
+
+        <div>
+            <div className='no_of_items'>
+                <h4>Items:</h4>
+                <p>{cart_items && cart_items.reduce((total,itm)=>total+itm.quantity,0)}</p>
+            </div>
+            <div className='tot_price'>
+                <h4>Total:</h4>
+                <p>₹ {cart_items && cart_items.reduce((total,itm)=>total+Number(itm.price)*Number(itm.quantity),0)}</p>
+            </div>
+            <div className="payment">
+                <h4>Payment:</h4>
+                <button>GPay</button>
+            </div>
+        </div>
+
         </div>
     )
 };
