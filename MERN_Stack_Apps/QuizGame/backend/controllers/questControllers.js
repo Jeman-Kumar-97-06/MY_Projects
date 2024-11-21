b15458cca497150e8fcea23f19e2caa2e4f236e4
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-11-21 23:19:44 +0530

    Made Changes

--- MERN_Stack_Apps/QuizGame/backend/controllers/questControllers.js
+++
@@ -1,9 +0,0 @@
-const Quest     = require('../models/questModel');
-const mongoose  = require('mongoose');
-
-const getQuestions = async (req,res) => {
-    const ten_quests = await Quest.aggregate([{$sample:{size:10}}]);
-    res.status(200).json(ten_quests);
-}
-
-module.exports = {getQuestions};
