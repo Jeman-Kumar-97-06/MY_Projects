1cb8c07355db8a12f8f2bfe50a39709726c5f491
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-11-26 23:18:08 +0530

    Deleted some files

--- MERN_Stack_Apps/AI_App/backend/models/popularPromptModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-
-const popularPromptsSchema = new Schema({
-    prompt : {type:String,required:false},
-    user_id : {type:String,required:true}
-},{timestamps:true});
-
-module.exports = mongoose.model('aiprompt',popularPromptsSchema);
