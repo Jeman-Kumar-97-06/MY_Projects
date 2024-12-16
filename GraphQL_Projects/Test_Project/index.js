ae6c8e723ca6c432c9491bf88f06a455a341d859
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-12-16 17:58:54 +0530

    made changes

--- GraphQL_Projects/Test_Project/index.js
+++ GraphQL_Projects/Test_Project/index.js
@@ -9,11 +9,25 @@ const resolvers = {
             return db.games; //--> db.games must be an array in which each object has an ID, title, and platform
                              //    just like we defined 'type Game' in schema.js
         },
+        game(){
+            return db.games.find((game)=>(game.id === game.id2))
+        },
         authors(){
             return db.authors;
         },
+        author(_,args){
+            return db.authors.find((author)=>(author.id === author.id));
+        },
         reviews(){
             return db.reviews;
+        },
+        review(_,args){
+            return db.reviews.find((review)=> (review.id === args.id))
+        }
+    },
+    Game: {
+        reviews(parent){
+            
         }
     }
 }
