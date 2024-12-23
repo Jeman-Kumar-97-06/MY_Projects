47321d643481a11282f3e6c754d8495e6261be2d
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-12-23 23:05:56 +0530

    Started doing Video CMS App

---
+++ MERN_Stack_Apps/UTube/backend/server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express  = require('express');
+const app      = express();
+const mongoose = require('mongoose');
+const cors     = require('cors');
+
+//Used to Enable CORS in Express App.
+app.use(cors());
+//Used to access req.body as JSON object.
+app.use(express.json());
+
+app.use('/api/videos',videoRoutes);
+app.use('/api/users',userRoutes)
+
+mongoose.connect(process.env.MONGOURL).then(()=>{app.listen(process.env.PORT,()=>{console.log("Request Listening At a PORT")})}).catch(err=>{console.log(err)});
