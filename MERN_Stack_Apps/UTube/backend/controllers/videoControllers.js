4996b44bea78cd751498d90bfc70f3303cb89b06
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-12-25 22:51:55 +0530

    Made Changes

--- MERN_Stack_Apps/UTube/backend/controllers/videoControllers.js
+++ MERN_Stack_Apps/UTube/backend/controllers/videoControllers.js
@@ -23,3 +23,30 @@ const getVideo = async (req,res) => {
     }
 }
 
+//Letting an Authorized User Upload Video :
+const uploadVideo = async (req,res) => {
+    const path = req.file;
+    try {
+        user_id = req.user_id;
+        const new_vid = await Video.create({video:path,user_id:user_id});
+        res.status(200).json({new_vid});
+    }
+    catch (err) {
+        res.status(404).json({error:err.message});
+    }
+};
+
+//Letting an Autorized User Delete a Video :
+const deleteVideo = async (req,res) => {
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"The ID is Invalid!"});
+    }
+    const vid_to_del = await Video.findOneAndDelete({_id:id});
+    if(!vid_to_del) {
+        return res.status(404).json({error:"No Video found with the matching ID!"})        
+    } 
+    res.status(200).json(vid_to_del);
+}
+
+module.exports = {getVideos,getVideo,uploadVideo};
