2b04a3c95cd053867851736877dba3e3da3c56f3
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2024-12-26 22:50:08 +0530

    made Changes

--- MERN_Stack_Apps/UTube/backend/routes/videoRoutes.js
+++ MERN_Stack_Apps/UTube/backend/routes/videoRoutes.js
@@ -5,4 +5,20 @@ const {getVideos,
        deleteVideo
     }         = require('../controllers/videoControllers');
 
+
+//If Authorized user sends a request the following 'requireAuth' will attach 'user_id' to the 'request'.
 const requireAuth = require('../middleware/requireAuth');
+
+const router = express.Router();
+
+router.use(requireAuth);
+
+router.get('/',getVideos);
+
+router.get("/:id",getVideo);
+
+router.post('/',uploadVideo);
+
+router.delete('/:id',deleteVideo);
+
+module.exports = router;
