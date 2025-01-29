ba5cde3863973dc6e49860fb54f0503befaebebb
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-01-29 20:56:56 +0530

    made changes

--- MERN_Stack_Apps/UTube/backend/models/videoModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
-
-const videoSchema = new Schema({
-    video : {type:String,required:true},
-    user_id : {type:String,required:true}
-},{timestamps:true});
-
-module.exports = mongoose.model("CMSVideo",videoSchema);
