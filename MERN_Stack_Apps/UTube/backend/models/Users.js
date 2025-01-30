6ae8e5447f1f07d7c28e016d68573a1ae57f3c44
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-01-30 16:37:57 +0530

    made changes

--- MERN_Stack_Apps/UTube/backend/models/Users.js
+++
@@ -1,8 +0,0 @@
-const mongoose  = require('mongoose');
-const usrSchema = new mongoose.Schema({
-    username : String,
-    email : String,
-    password : String
-});
-
-module.exports = mongoose.model('User',usrSchema);
