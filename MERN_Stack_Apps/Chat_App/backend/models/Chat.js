12f607793eb4f7d3c8e44bb5f22d526e7dec7553
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-02-03 22:27:02 +0530

    Made Changes

---
+++ MERN_Stack_Apps/Chat_App/backend/models/Chat.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const ChatSchema = new mongoose.Schema({
+    sender : String,
+    message : String,
+    timestamp : {type:Date, default: Date.now}
+});
+
+export default mongoose.model("Chat",ChatSchema);
---
+++ MERN_Stack_Apps/Chat_App/backend/server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const { Server } = require('socket.io')
+const cors       = require('cors');
+const exp        = require('express');
+const app        = exp();
+const charRt     = require('./routes/chats');
+const usrRt      = require('./routes/users');
+const mong       = require('mongoose');
+const http       = require('http');
+const server     = http.createServer(app);
+const io         = new Server(server, {cors: {origin:"*"}});
+
+app.use(exp.json());
+app.use(cors());
+
+
+io.on("connection",(socket)=>{
+    socket.on("sendMessage",(message)=>{
+        io.emit('receiveMessage',message);
+    })
+})
+
+mong.connect(process.env.MONGOURL).then(
+    () => {
+        app.listen(process.env.PORT,()=>{
+            console.log(`Connected & Listening at ${process.env.PORT}`)
+        })
+    }
+)
+.catch(err=>{console.log(err)});
