32f930a51a533e2def0935e9a95d392fb5f5923e
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-02-04 18:03:50 +0530

    made Changes

--- MERN_Stack_Apps/Chat_App/backend/models/User.js
+++ MERN_Stack_Apps/Chat_App/backend/models/User.js
@@ -1,8 +1,22 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+
 const UserSchema = new mongoose.Schema({
     username : String,
     email : String,
     password : String
 });
+
+UserSchema.statics.login = async function (name,email,password) {
+    if (!name || !email || !password) {
+        throw Error("All fields must be filled!")
+    }
+    if (!validator)
+}
+
+UserSchema.statics.signup = () => {
+
+}
 
 export default mongoose.model("User",UserSchema);
