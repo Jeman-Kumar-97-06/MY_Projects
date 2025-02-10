6182d94ec9b1acf0b1fa2359e38d21c223ee36a3
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-02-10 10:24:20 +0530

    made Changes

--- MERN_Stack_Apps/Chat_App/frontend/src/App.jsx
+++ MERN_Stack_Apps/Chat_App/frontend/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1 class="text-3xl font-bold underline">Hello world!s</h1>
-    </>
+    <div className='p-4'>
+      <Signup/>
+    </div>
   )
 }
 
