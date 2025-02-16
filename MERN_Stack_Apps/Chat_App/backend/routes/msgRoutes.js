dcfb50b818bd3dd735a5360b351ebada95cc21ab
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-02-16 21:22:37 +0530

    Made Changes

--- MERN_Stack_Apps/Chat_App/backend/routes/msgRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const {sendMessage, getMessages} = require('../controllers/msgControllers');
-const protectRoutes = require('../middleware/requireAuth');
-const router  = express.Router();
-
-router.get('/:id',protectRoutes,getMessages);
-router.post('/send/:id',protectRoutes,sendMessage);
-
-module.exports = router;
