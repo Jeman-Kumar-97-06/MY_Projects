4f5e376b946ab5b5fd73e9aa36568f242bb30366
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-02-26 21:49:46 +0530

    made changes

--- MERN_Stack_Apps/Chat_App/backend/controllers/messageControllers.js
+++ MERN_Stack_Apps/Chat_App/backend/controllers/messageControllers.js
@@ -1,12 +1,47 @@
+const Conversation = require('../models/conversationModel');
+const Message      = require('../models/messageModel');
 const sendMessage = async (req,res) => {
     try {
         const {message} = req.body;
-        const {id}      = req.params;//The id of the person receiving the message
-        const senderId  = req.userId;//The id of the person sending the message
+        const {id:receiverId}      = req.params;//The id of the person receiving the message
+        const senderId  = req.user._id;//The id of the person sending the message [we get this from the req object after the requireAuth is executed]
+        let conversation = await Conversation.findOne({
+            participants : {$all:[senderId,receiverId]}
+        });
+
+        if (!conversation) {//if there isn't a conversation create one:
+            conversation = await Conversation.create({
+                participants : [senderId,receiverId]
+            })
+        }
+        const newMessage = new Message({
+            senderId:senderId,
+            receiverId : receiverId,
+            message : message
+        })
+        if (newMessage) {
+            conversation.messages.push(newMessage._id);
+        }
+        //Socket functionality will go here :
+        await conversation.save();
+        await newMessage.save();
+        res.status(201).json(newMessage);
     } catch (error) {
         console.log("Error in sendMessage Controller: ",error.message);
         res.status(500).json({error:"Internal Server Error"});
     }
 }
 
-module.exports = {sendMessage}
+const getMessages = async (req,res) => {
+    try {
+        const {id:userToChatId} = req.params;
+        const senderId = req.user._id;
+        const conversation = await Conversation.findOne({
+            participants : {$all : [senderId,userToChatId]}
+        })
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {sendMessage,getMessages}
--- MERN_Stack_Apps/Chat_App/backend/routes/messageRoutes.js
+++ MERN_Stack_Apps/Chat_App/backend/routes/messageRoutes.js
@@ -1,9 +1,13 @@
 import { sendMessage } from '../controllers/messageControllers';
+import requireAuth from '../middleware/requireAuth';
 
 const express = require('express');
 
 const router  = express.Router();
 
+router.use(requireAuth);
+
+router.get('/:id',getMessage);
 router.post('/send/:id',sendMessage);
 
 export default router;
