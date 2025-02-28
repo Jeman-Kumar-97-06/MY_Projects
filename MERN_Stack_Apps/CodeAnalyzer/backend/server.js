f4a4047952dee89cc01b6d88e53428ea7a20f210
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-02-28 23:02:34 +0530

    Made changes to backend code

--- MERN_Stack_Apps/CodeAnalyzer/backend/server.js
+++ MERN_Stack_Apps/CodeAnalyzer/backend/server.js
@@ -4,5 +4,13 @@ const app                       = express();
 const {GoogleGenerativeAI:ggAI} = require('@google/generative-ai');
 const genAI                     = new ggAI(`${process.env.GEMINI_K}`);
 const model                     = genAI.getGenerativeModel({model:"gemini-1.5-flash"});
+const cors                      = require('cors')
+
+app.use(express.json());
+app.use(cors());
+
+app.post('/api/askAI',(req,res)=>{
+    console.log(req.body)
+})
 
 app.listen(process.env.PORT,()=>{console.log("Listening at ",process.env.PORT)});
