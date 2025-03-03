af424d7a0d933364628d7e8412c4b20b99bfb60e
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-03-03 23:35:35 +0530

    Made Changes

---
+++ MERN_Stack_Apps/Chat_App/frontend/src/hooks/useLogin.js
@@ -0,0 +1,28 @@
+import {useState} from 'react';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogin = () => {
+    const [error,setError] = useState(null);
+    const [isloading,setIsloading] = useState(null);
+    const {dispatch} = useAuthContext();
+    const login = async (username,password) => {
+        setIsloading(true);
+        setError(null);
+        const resp = await fetch('/api/auth/login',{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify({username,password})
+        });
+        const json = await resp.json();
+        if (!resp.ok) {
+            setIsloading(false);
+            setError(json.error);
+        }
+        if (resp.ok) {
+            setIsloading(false);
+            localStorage.setItem("chatuser",JSON.stringify(json));
+            dispatch({type:"LOGIN",payload:user});
+        }
+    };
+    return {login,error,isloading};
+}
--- MERN_Stack_Apps/Chat_App/frontend/src/hooks/useSignup.js
+++ MERN_Stack_Apps/Chat_App/frontend/src/hooks/useSignup.js
@@ -8,14 +8,21 @@ export const useSignup = () => {
     const signup = async (fullname,username,password,gender) => {
         setIsloading(true);
         setError(null);
-        const resp = await fetch('/',{
+        const resp = await fetch('/api/auth/signup',{
             method:"POST",
             headers:{"Content-Type":"application/json"},
             body:JSON.stringify({fullname,username,password,gender})
         });
         const json = await resp.json();
-        if (!resp.ok) {
+        if (!resp.ok){
+            setIsloading(false);
+            setError(json.error);
+        }
+        if (resp.ok)    {
+            localStorage.setItem('chatuser',JSON.stringify(json));
+            dispatch({type:"LOGIN",payload:json});
             setIsloading(false);
         }
     }
+    return {signup,error,isloading}
 }
