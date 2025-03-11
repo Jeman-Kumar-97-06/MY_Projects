8f1a26615f60c5c7f95f773efe169e14085db7be
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-03-11 12:10:42 +0530

    made changes

--- MERN_Stack_Apps/WallpaperApp/backend/server.js
+++ MERN_Stack_Apps/WallpaperApp/backend/server.js
@@ -3,6 +3,7 @@ const express  = require('express');
 const app      = express();
 const mongoose = require('mongoose');
 const cors     = require('cors');
+const path     = require('path');
 
 const userRts  = require('./routes/users');
 const wallRts  = require('./routes/walls');
