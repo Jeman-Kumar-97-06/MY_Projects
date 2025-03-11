331ba00650898b9dcc00af5813eabc1b7932bc04
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-03-11 13:08:25 +0530

    Made Changes

--- MERN_Stack_Apps/WallpaperApp/backend/controllers/wallControllers.js
+++ MERN_Stack_Apps/WallpaperApp/backend/controllers/wallControllers.js
@@ -32,7 +32,6 @@ const getWall = async (req,res) => {
 
 //Letting an Authenticated User Upload wallpaper
 const uploadWalls = async (req,res) => {
-    console.log(req)
     if (!req.file) {
         return res.status(400).json({error:"Invalid file type! only JPEG allowed."});
     }
