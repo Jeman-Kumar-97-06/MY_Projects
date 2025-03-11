5d8462652aed037b449a16c5033cf758f7bb01b8
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-03-11 14:47:21 +0530

    Made changes

--- MERN_Stack_Apps/WallpaperApp/frontend/src/components/Navbar.jsx
+++ MERN_Stack_Apps/WallpaperApp/frontend/src/components/Navbar.jsx
@@ -15,12 +15,12 @@ const Navbar = () => {
         <div class="text-xl font-bold">Wallpicks</div>
       
         
-        <div class="flex-1 mx-4 max-w-lg">
+        {/* <div class="flex-1 mx-4 max-w-lg">
         <input 
             type="text" 
             placeholder="Search..." 
             class="w-full px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />    
+          />     */}
             {/* <form class="max-w-md mx-auto">   
                 <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                 <div class="relative">
@@ -33,7 +33,7 @@ const Navbar = () => {
                     <button type="submit" class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                 </div>
             </form> */}
-        </div>
+        {/* </div> */}
       
         <div class="flex items-center space-x-4">
           {
