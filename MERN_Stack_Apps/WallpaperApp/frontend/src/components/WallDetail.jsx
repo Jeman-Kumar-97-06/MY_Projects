f19956626f999dafa485718960a819077dfb76cb
Jeman-Kumar-97-06 <jeman060397@gmail.com>
2025-03-11 14:53:51 +0530

    made changes

--- MERN_Stack_Apps/WallpaperApp/frontend/src/components/WallDetail.jsx
+++ MERN_Stack_Apps/WallpaperApp/frontend/src/components/WallDetail.jsx
@@ -3,12 +3,12 @@ const WallDetail = ({w}) => {
     let fullUrl = `https://my-projects-lxja.onrender.com/${w.wall}`
 
     return (
-        <div class="max-w-sm mr-12 mt-6 h-auto min-h-3 p-6 bg-gray-100 rounded-lg shadow-lg">
-        <div class="relative">
+        <div class="max-w-sm mr-12 mt-6 h-auto min-h-1 p-6 bg-gray-100 rounded-lg shadow-lg">
+        <div class="h-auto relative">
           <img 
             src={fullUrl} 
             alt="Wallpaper" 
-            class="w-full h-auto rounded-lg"
+            class="w-full aspect-[3/4] object-cover rounded-lg"
           />
     
           <a 
