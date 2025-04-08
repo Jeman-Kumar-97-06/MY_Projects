87866bdcbc2fb90374e724172500400a320d8212
Jeman <152058176+Jeman-Kumar-97-06@users.noreply.github.com>
2025-04-08 12:43:31 +0530

    Add files via upload

---
+++ MERN_Stack_Apps/Refurbished_App/backend/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const cors     = require('cors');
+const express  = require('express');
+const mongoose = require('mongoose');
+const app      = express();
+
+const uRts     = require('./routes/users');
+const cRts     = require('./routes/carts');
+const pRts     = require('./routes/products');
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/api/users',uRts);
+app.use('/api/carts',cRts);
+app.use('/api/products',pRts);
+
+mongoose.connect(process.env.MONGOURL).then(()=>{
+  app.listen(process.env.PORT,()=>{console.log("Connected to database and listening to request at ",process.env.PORT)})
+}).catch(error=>{console.log(error)})
